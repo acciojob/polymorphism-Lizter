a3ac1bc6cebe5eb7a8a66626080d50df66def299
Lizter <lizterdsouza@gmail.com>
2022-12-14 00:40:52 +0530

    code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    Product p= new Product();
+    p.product(20,30);
+    p.product(20,30,40);
+    p.product(10.5,9.9);
 
 }
+    class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
