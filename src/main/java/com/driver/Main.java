672725bd47ab4b4c5ad0fb423aa7f4c0bf43190c
Lizter <lizterdsouza@gmail.com>
2022-12-14 00:52:48 +0530

    code4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,23 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
         Product p = new Product();
-        int ans=p.product(20, 30);
-        int ans1=p.product(20, 30, 40);
-        double ans2=p.product(10.5, 9.9);
-        System.out.println(ans+" " + ans1 + " "+ ans2);
+        int ans = p.product(20, 30);
+        int ans1 = p.product(20, 30, 40);
+        double ans2 = p.product(10.5, 9.9);
+        System.out.println(ans + " " + ans1 + " " + ans2);
     }
-}
-    public static class Product{
+
+    public static class Product {
         public int product(int x, int y) {
-            return x*y;
+            return x * y;
         }
 
         public int product(int x, int y, int z) {
-            return x*y*z;
+            return x * y * z;
         }
 
         public double product(double x, double y) {
-            return x*y;
+            return x * y;
         }
     }
+}
 
